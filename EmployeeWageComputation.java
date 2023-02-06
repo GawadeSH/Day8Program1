933b10d4a0e3bb69d84b0a526f7fb92cf1769b2a
shital gawade <shitalgawade131@gmail.com>
2023-02-06 12:33:31 +0530

    Welcome to Program

---
+++ EmployeeWageComputation.java
@@ -0,0 +1,9 @@
+package wage;
+
+public class EmployeeWageComputation
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Employee Wage Computation Program");
+    }
+}
